28b5a97678b48704d1f305be1b361a2c610fabbe
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2018-06-30 05:24:18 +0100

    add nmap

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ echo
 echo "All built, below are relevant aliases"
 for imageName in *; do
     if [ -d "$imageName" -a \( "$1" = "" -o "$imageName" == "$1" \) ]; then
-        echo "alias $imageName=\"docker run --rm -i --name $imageName -v \$(pwd):/workdir -w /workdir $imageName\""
+        echo "alias $imageName='docker run --rm -i --name $imageName -v \$(pwd):/workdir -w /workdir $imageName'"
     fi
 done
